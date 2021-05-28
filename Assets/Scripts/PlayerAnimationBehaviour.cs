6d7f7f78b0c0894a5d4fd84c4256ab1db5ad7fcf
JosephPena1 <jgpena2888@gmail.com>
2021-05-28 11:07:56 -0500

    Made character animated;

---
+++ Assets/Scripts/PlayerAnimationBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationBehaviour : MonoBehaviour
+{
+    private PlayerMovementBehaviour _movementBehaviour;
+    [SerializeField]
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Initializes the move component reference
+        _movementBehaviour = GetComponent<PlayerMovementBehaviour>();
+    }
+
+    public void ActivateWinTrigger()
+    {
+        _animator.SetTrigger("WinGame");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _animator.SetFloat("Speed", _movementBehaviour.Velocity.magnitude);
+        _animator.SetBool("InAir", !_movementBehaviour.IsGrounded);
+    }
+}
